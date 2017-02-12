dd57de83156f52d4db84d77e2ccdd65b1c02a7dd
stevexe <stevexe@gmail.com>
2017-02-12 12:07:55 -0800

    added grey/green 5th color

--- EarthColors.py
+++ EarthColors.py
@@ -4,6 +4,7 @@ import csv
 im = Image.open('earthmap360x180.jpg')
 pix = im.load()
 
+#if the image isn't already 360x180 for nice and easy 1px=1deg mapping
 #x_inc = 3600/im.size[0]
 #y_inc = 1800/im.size[1]
 
@@ -17,27 +18,27 @@ for y in range (0,180):
 		g = float(pix[x,y][1])
 		b = float(pix[x,y][2])
 		
-		#hex color of each pixel rounded to bin values (3 bins, 27 colors)
+		#hex value color of each pixel rounded to bin values (3 bins, 27 colors) for a more detailed palette 
 		hr = str(hex(int(round((r)/127.5) * 127.5))).replace('0x','')
 		hg = str(hex(int(round((g)/127.5) * 127.5))).replace('0x','')
 		hb = str(hex(int(round((b)/127.5) * 127.5))).replace('0x','')
 		hx = hr + hg + hb
 			
 			
-		#rough Blue, White, Green, Brown color divisions base on rgb values
+		#rough Blue, White, Green, Brown color bins based on rgb values
 		if r > 200 and g > 200 and b > 200:
-			color = 'w'
-		elif abs(r-g) < 40 and (r+g) > 400 and b > 150:
-			color = 'br'
+			color = 'w' #white
+		elif abs(r-g) < 20 and (r+g) > 400 and b > 150:
+			color = 'br' #brown/tan
 		elif b > g and b > r:
-			color = 'b'
+			color = 'b' #blue
 		elif g > b and g > r:
-			color = 'g'
+			color = 'g' #green
 		elif r > b and r > g:
-			color = 'g'
+			color = 'gr' #grey/green
 		else:
-			color = 'w'
-		#print(x-180,y-90,r,g,b,color)
+			color = 'w' #white
+
 		row = [(x-180),-1*(y-90),color,hx]
 		out_data.append(row)
 		
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Examines jpg image pixel colors and bins into four "Earth colors" (blue, white, green, brown). Outputs pixel coords (x,y = lon,lat) and color values.
+Examines jpg image pixel colors and bins into five "Earth colors" (blue, white, green, brown, grey/green). Outputs pixel coords (x,y = lon,lat) and color values.
 
 Requires: csv, Pillow (PIL fork) https://pillow.readthedocs.io/en/4.0.x/
